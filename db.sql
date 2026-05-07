f60e66109bba0f5771adfd8f0cb8d0e26fe468c3
ubuntu-vm <lybahoang@ubuntu>
2026-05-07 11:06:10 +0700

    add db.sql file which create database 'socialnet' and table 'account'

---
+++ db.sql
@@ -0,0 +1,13 @@
+-- Create the database
+CREATE DATABASE IF NOT EXISTS socialnet;
+USE socialnet;
+
+-- Create the account table
+CREATE TABLE IF NOT EXISTS account (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR(50) NOT NULL UNIQUE,
+	fullname VARCHAR(100) NOT NULL,
+    	password VARCHAR(255) NOT NULL,
+	description TEXT NULL
+);
+
