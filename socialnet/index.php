6f6aceaf0615f3906349eaa244d666fb32815f7b
lybahoang <157574648+lybahoang@users.noreply.github.com>
2026-05-10 10:01:14 +0700

    modify index.php to handle adding friends

--- socialnet/index.php
+++ socialnet/index.php
@@ -330,7 +330,7 @@ else
                         <a href="friendship/accept_friend.php?requesterID=<?= $user['id']?>" class="view-btn">
                             Accept
                         </a>
-                        <a href="friendship/reject_friend_request.php?requesterID=<?= $user['id']?>" class="view-btn">
+                        <a href="friendship/reject_friend_request.php?targetID=<?= $user['id']?>" class="view-btn">
                             Do not accept
                         </a>
                     </td>
@@ -386,7 +386,7 @@ else
                         Pending
                     </td>
                     <td>
-                        <a href="friendship/reject_friend_request.php?requesterID=<?= $user['id']?>" class="view-btn">
+                        <a href="friendship/reject_friend_request.php?targetID=<?= $user['id']?>" class="view-btn">
                             Undo
                         </a>
                     </td>
